e9e0642599409b25dee591a782d6eb1f02d1773f
Eric Ray <erichamion@gmail.com>
2022-04-21 19:35:47 -0500

    Add project files.

---
+++ ExceptionsScratchpad/Calculator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsScratchpad
+{
+    public class Calculator
+    {
+        public bool IsCalculating { get; private set; }
+
+        public int Calculate(string oper, int a, int b)
+        {
+            IsCalculating = true;
+
+            int result;
+            try
+            {
+
+
+                switch (oper)
+                {
+                    case "+":
+                        result = Add(a, b);
+                        break;
+                    case "-":
+                        result = Subtract(a, b);
+                        break;
+                    case "*":
+                        result = Multiply(a, b);
+                        break;
+                    case "/":
+                        result = Divide(a, b);
+                        break;
+                    default:
+                        throw new ArgumentException($"'{oper}' is not a valid operator");
+                }
+            }
+            finally
+            {
+                IsCalculating = false;
+            }
+
+            return result;
+        }
+
+        private int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        private int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        private int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        private int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ ExceptionsScratchpad/Program.cs
@@ -0,0 +1,74 @@
+﻿using ExceptionsScratchpad;
+
+
+Calculator calc = new Calculator();
+
+Console.Write("Enter the first number: ");
+string leftString = Console.ReadLine();
+int left = int.Parse(leftString);
+
+Console.Write("Enter the operator: ");
+string oper = Console.ReadLine();
+
+Console.Write("Enter the second number: ");
+string rightString = Console.ReadLine();
+int right = int.Parse(rightString);
+
+
+try
+{
+    int result = calc.Calculate(oper, left, right);
+    Console.WriteLine($"Your result is {result}");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Can't divide by zero!");
+}
+catch (Exception)
+{
+    Console.WriteLine("Something went wrong!");
+}
+finally
+{
+    Console.WriteLine("End of try");
+}
+
+Console.WriteLine(calc.IsCalculating);
+
+//Console.WriteLine("Enter a number:");
+//string s = Console.ReadLine();
+//try
+//{
+//    int n = int.Parse(s);
+
+//    if (n == 13)
+//    {
+//        throw new UnluckyNumberException();
+//    }
+//    Console.WriteLine(100 / n);
+//}
+//catch (FormatException)
+//{
+//    Console.WriteLine("That's not a number!");
+//    //Console.WriteLine(ex.Message);
+
+//}
+//catch (DivideByZeroException)
+//{
+//    Console.WriteLine("Can't divide by zero!");
+//}
+//catch (UnluckyNumberException)
+//{
+//    Console.WriteLine("That number is unlucky");
+//}
+
+
+
+
+//Console.WriteLine("End");
+////string s = null;
+////Console.WriteLine(s.Length);
+
+////int numerator = 42;
+////int denominator = 0;
+////Console.WriteLine(numerator / denominator);
